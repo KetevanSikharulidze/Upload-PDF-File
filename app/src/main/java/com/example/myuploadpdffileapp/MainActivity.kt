20d530806ea42c6372db1982df23f74321b7a5a0
student <student@BTU.local>
2025-11-06 14:27:02 +0400

    select PDF file from phone and show title

--- app/src/main/java/com/example/myuploadpdffileapp/MainActivity.kt
+++ app/src/main/java/com/example/myuploadpdffileapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.myuploadpdffileapp
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.documentfile.provider.DocumentFile
+import com.example.myuploadpdffileapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    private var pdfFileUri: Uri?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.selectedPdfButton.setOnClickListener {
+
+            launcher.launch("application/pdf")
+//            launcher.launch("image/*")
+
+        }
+
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.GetContent()){uri->
+        pdfFileUri = uri
+
+        val fileName = uri?.let { DocumentFile.fromSingleUri(this, uri)?.name }
+        binding.selectedOrNotTextView.text = fileName.toString()
+
+
     }
 }
